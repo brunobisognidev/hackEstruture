0b14036ada376429aadd86222f6e2e0103f5d00a
Bruno De A. Bisogni <bruno@levoo.com.br>
2019-12-16 15:32:16 -0300

    Estrutura do front instalada

--- Levaai/controller/user.js
+++ Levaai/controller/user.js
@@ -17,5 +17,26 @@ exports.read =(req, res) => {
    req.profile.hashed_password = undefined;
    req.profile.salt = undefined; 
    return res.json(req.profile); 
-}
+};
+
+exports.update = (req, res) => {
+   user.findByIdAndUpdate(
+      {_id: req.profile._id },
+      {$set:  req.body },
+      { new: true },
+      (err, user) => {
+         if (err) {
+            return res.status(400).json({
+               error: "You are authorized to perform this action"
+            }); 
+         }
+         user.hashed_password = undefined;
+         user.salt = undefined;
+         res.json(user);
+         
+      }
+   );
+}; 
+
+
 
